a13472f8c9e037812cf85d8ce44755ad2563a30d
Antoine Kingue <etorefr@gmail.com>
2022-09-06 22:31:21 +0200

    🎉 initial commit

---
+++ font-packages/plus-jakarta-sans/index.d.ts
@@ -0,0 +1,12 @@
+export { useFonts } from './useFonts';
+
+export const __metdata__: Any;
+export const PlusJakartaSans_100Thin: number;
+export const PlusJakartaSans_200ExtraLight: number;
+export const PlusJakartaSans_300Light: number;
+export const PlusJakartaSans_400Regular: number;
+export const PlusJakartaSans_500Medium: number;
+export const PlusJakartaSans_600SemiBold: number;
+export const PlusJakartaSans_700Bold: number;
+export const PlusJakartaSans_800ExtraBold: number;
+export const PlusJakartaSans_900Black: number;
---
+++ font-packages/plus-jakarta-sans/index.js
@@ -0,0 +1,12 @@
+export { useFonts } from './useFonts';
+
+export { default as __metadata__ } from './metadata.json';
+export const PlusJakartaSans_100Thin = require('./PlusJakartaSans_100Thin.ttf');
+export const PlusJakartaSans_200ExtraLight = require('./PlusJakartaSans_200ExtraLight.ttf');
+export const PlusJakartaSans_300Light = require('./PlusJakartaSans_300Light.ttf');
+export const PlusJakartaSans_400Regular = require('./PlusJakartaSans_400Regular.ttf');
+export const PlusJakartaSans_500Medium = require('./PlusJakartaSans_500Medium.ttf');
+export const PlusJakartaSans_600SemiBold = require('./PlusJakartaSans_600SemiBold.ttf');
+export const PlusJakartaSans_700Bold = require('./PlusJakartaSans_700Bold.ttf');
+export const PlusJakartaSans_800ExtraBold = require('./PlusJakartaSans_800ExtraBold.ttf');
+export const PlusJakartaSans_900Black = require('./PlusJakartaSans_900Black.ttf');
